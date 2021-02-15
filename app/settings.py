443e99b8fa7370abfb15ad0e24cfacd329e9f0d7
Vasia Pupkin <zimovchanin@gmail.com>
2021-02-15 14:11:01 +0300

    Setup postgresql

--- app/settings.py
+++ app/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),
     }
 }
 
