f1bd522cc0c649de18b9d799caebf80d158aa9cb
ShurikBeliy <rucodextream@gmail.com>
2021-02-17 14:59:28 +0300

    Add base class for views and page 404 view

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', IndexDetail.as_view(), name='index_detail_url'),
+    path('404/', Page404Detail.as_view(), name='page_404_url'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.views.generic import View
+import logging
 
-class IndexDetail(View):
+logger = logging.getLogger(__name__)
+
+class BaseView(View):
+    """ Base view class that try to catch uncaught exceptions and log them """
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            response = super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            extra={'method':request.method, 'url': request.build_absolute_uri()}
+            logger.error('Exception is: {}'.format(e), extra)
+            return redirect('page_404_url')
+
+        if isinstance(response,(dict,list)):
+            return self._response(response)
+        return response
+
+    @staticmethod
+    def _response(data, status=200):
+        """ Return json response """
+        return JsonResponse(
+            data,
+            status=status,
+            safe=not isinstance(data, list)
+        )
+
+class Page404Detail(BaseView):
+    """ View for page 404 """
+    def get(self, request):
+        response = render(request, 'catalog/404.html', {})
+        response.status_code = 404
+        return response
+
+class IndexDetail(BaseView):
+    """ View for index page """
     def get(self, request):
         return render(request, 'catalog/index.html', {})
