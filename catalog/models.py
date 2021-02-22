3dd14e5accc0745dde60104d204cb292bd64ed5e
ShurikBeliy <rucodextream@gmail.com>
2021-02-22 12:23:04 +0300

    Add Item model

--- catalog/models.py
+++ catalog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Category(models.Model):
     """ Caegories of items """
+    is_active = models.BooleanField('Is active', default=False, db_index=True)
     priority = models.PositiveIntegerField('Priority')
     name = models.CharField('Category name', max_length=150)
     slug = models.SlugField('Slug', max_length=50, allow_unicode=True, db_index=True)
-    is_active = models.BooleanField('Is active', default=False, db_index=True)
 
     def __str__(self):
         return '%s' % (self.name)
@@ -14,4 +14,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+        ordering = ('priority',)
+
+class Item(models.Model):
+    """ Items """
+    is_active = models.BooleanField('Is active', default=False, db_index=True)
+    priority = models.PositiveIntegerField('Priority')
+    name = models.CharField('Item', max_length=150)
+    slug = models.SlugField('Slug', max_length=50, allow_unicode=True, db_index=True)
+    description = models.TextField('Description', blank=True)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
         ordering = ('priority',)
