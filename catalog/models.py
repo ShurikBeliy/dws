3f0cf8d2d2e34f3e434fb6e22fba9f496dd5d239
ShurikBeliy <rucodextream@gmail.com>
2021-02-22 12:00:18 +0300

    Add Category model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Caegories of items """
+    priority = models.PositiveIntegerField('Priority')
+    name = models.CharField('Category name', max_length=150)
+    slug = models.SlugField('Slug', max_length=50, allow_unicode=True, db_index=True)
+    is_active = models.BooleanField('Is active', default=False, db_index=True)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ('priority',)
