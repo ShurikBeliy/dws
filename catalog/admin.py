86950939862dc1067d9b08c168d1305bfdaefdaa
ShurikBeliy <rucodextream@gmail.com>
2021-02-22 12:15:03 +0300

    Add CategoryAdmin model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('is_active', 'slug', 'name', 'priority',)
+    list_display_links = ('name',)
+    list_editable = ('is_active', 'priority',)
+    search_fields = ('name','slug',)
