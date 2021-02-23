3eac1ac4b1a7b84085a6d1c3db4895c4d3dcfa38
ShurikBeliy <rucodextream@gmail.com>
2021-02-23 20:13:09 +0300

    Better to use functions for services - less code

--- catalog/services/category.py
+++ catalog/services/category.py
@@ -1,25 +1,20 @@
 from catalog.models import Category, Item
 
-class CagegoryService:
-    """ Services for categories """
-    @staticmethod
-    def get_data(slug):
-        """ Get all data of category with slug that we give """
-        category = CagegoryService.get_active_categry_by_slug(slug)
-        return {
-                "category": category,
-                "items": CagegoryService.get_categry_active_items(category.id)
-        }
+def get_category_active_data(slug):
+    """ Get all data of category with slug that we give """
+    category = get_active_categry_by_slug(slug)
+    return {
+            "category": category,
+            "items": get_categry_active_items(category.id)
+    }
 
-    @staticmethod
-    def get_active_categry_by_slug(slug):
-        """ Get categry that active and has slug that we give """
-        return Category.objects.get(is_active=True, slug=slug)
+def get_active_categry_by_slug(slug):
+    """ Get categry that active and has slug that we give """
+    return Category.objects.get(is_active=True, slug=slug)
 
-    @staticmethod
-    def get_categry_active_items(category_id):
-        """ Get items that active and have category_id that we give """
-        try:
-            return Item.objects.filter(is_active=True, category_id=category_id)
-        except Exception as e:
-            return None
+def get_categry_active_items(category_id):
+    """ Get items that active and have category_id that we give """
+    try:
+        return Item.objects.filter(is_active=True, category_id=category_id)
+    except Exception as e:
+        return None
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from core.views import BaseView
-from .services.category import CagegoryService
+from .services.category import get_category_active_data
 
 class CategoryDetail(BaseView):
     """ View for category page """
     def get(self, request, slug):
         return render(request, 'catalog/category.html',
-                      CagegoryService.get_data(slug))
+                      get_category_active_data(slug))
