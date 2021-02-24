12b87c982bf8f55744de0cd08313c3dcb1242ff0
ShurikBeliy <rucodextream@gmail.com>
2021-02-24 11:41:16 +0300

    Fix some small mistakes

--- catalog/services/category.py
+++ catalog/services/category.py
@@ -3,18 +3,21 @@ from catalog.models import Category, Item
 def get_category_active_data(slug):
     """ Get all data of category with slug that we give """
     category = get_active_categry_by_slug(slug)
+    items = None
+    try:
+        items = get_active_items_by_category_id(category.id)
+    except:
+        pass
+
     return {
             "category": category,
-            "items": get_categry_active_items(category.id)
+            "items": items
     }
 
 def get_active_categry_by_slug(slug):
     """ Get categry that active and has slug that we give """
     return Category.objects.get(is_active=True, slug=slug)
 
-def get_categry_active_items(category_id):
+def get_active_items_by_category_id(category_id):
     """ Get items that active and have category_id that we give """
-    try:
-        return Item.objects.filter(is_active=True, category_id=category_id)
-    except Exception as e:
-        return None
+    return Item.objects.filter(is_active=True, category_id=category_id)
