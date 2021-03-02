1f76e289024edd0b82f158fb896065fd7b29487b
ShurikBeliy <rucodextream@gmail.com>
2021-03-02 12:50:56 +0300

    Add characteristic to items. We will use it as filter

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Category(models.Model):
     """ Categories of items """
     is_active = models.BooleanField('Is active', default=False, db_index=True)
@@ -16,6 +15,38 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
         ordering = ('priority',)
 
+class Characteristic(models.Model):
+    """ Characteristic for item If characteristic doesn't have category they are for all items
+        But it they have category they are for items in that category """
+    is_active = models.BooleanField('Is active', default=False, db_index=True)
+    priority = models.PositiveIntegerField('Priority')
+    name = models.CharField('Characteristic', max_length=150)
+    category = models.ForeignKey('category', verbose_name='Category', on_delete=models.CASCADE, related_name='characteristics', blank=True)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    class Meta:
+        verbose_name = 'Characteristic'
+        verbose_name_plural = 'Characteristics'
+        ordering = ('priority',)
+        unique_together = (('name', 'category',),)
+
+class CharacteristicValue(models.Model):
+    """ Values for characteristics of item """
+    is_active = models.BooleanField('Is active', default=False, db_index=True)
+    characteristic = models.ForeignKey('characteristic', verbose_name='Characteristic', on_delete=models.CASCADE, related_name='values')
+    item = models.ForeignKey('item', verbose_name='Item', on_delete=models.CASCADE, related_name='values')
+
+    def __str__(self):
+        return '%s' % (self.is_active)
+
+    class Meta:
+        verbose_name = 'Characteristic value'
+        verbose_name_plural = 'Characteristic values'
+        ordering = ('characteristic__priority',)
+        unique_together = (('characteristic', 'item',),)
+
 class Item(models.Model):
     """ Items """
     is_active = models.BooleanField('Is active', default=False, db_index=True)
@@ -23,7 +54,7 @@ class Item(models.Model):
     name = models.CharField('Item', max_length=150)
     slug = models.SlugField('Slug', max_length=50, allow_unicode=True, db_index=True)
     preview = models.ImageField('Preview', upload_to='item_preview/', blank=True)
-    category = models.ForeignKey('category', verbose_name='Category', on_delete=models.CASCADE, related_name='items', null=True)
+    category = models.ForeignKey('category', verbose_name='Category', on_delete=models.CASCADE, related_name='items')
     description = models.TextField('Description', blank=True)
 
     def __str__(self):
